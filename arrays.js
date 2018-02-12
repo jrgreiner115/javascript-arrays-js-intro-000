fd4b5cefcab6e2a77205e87f15c5fb5144a0528d
Joe Greiner <jrgreiner115@gmail.com>
2018-02-12 18:44:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,4 +27,4 @@ function destructivelyRemoveElementFromEndOfArray(array,element) {
   array.pop(element);
   return array;
 }
-function removeElementFromBeginningOfArray
+function removeElementFromEndOfArray
