72ac516e6cb6e3e8d337fdecdb7118510b88a990
Joe Greiner <jrgreiner115@gmail.com>
2018-02-12 17:09:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,6 @@ function addElementToBeginningOfArray(array, element) {
  return [element,...array];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element);
+  return array
 }
