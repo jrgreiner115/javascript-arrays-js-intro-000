e58a31bbc6b4b5512792f6b675c419699592c0f8
Joe Greiner <jrgreiner115@gmail.com>
2018-02-12 16:48:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(, ["Motts"]) {
+function addElementToBeginningOfArray(ingredients, ) {
   ["apple", "sauce"]
   
 }
